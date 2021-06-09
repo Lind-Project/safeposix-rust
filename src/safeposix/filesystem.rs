c1cb5f5653b6e7d28337d3ccfb20b0ad8c16af6d
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2021-06-09 15:14:23 -0400

    filesystem

--- src/safeposix/filesystem.rs
+++ src/safeposix/filesystem.rs
@@ -2,14 +2,48 @@
 
 use crate::interface;
 
+
+enum Inode {
+    File(GenericInode),
+    Dir(DirectoryInode),
+    Stream(GenericInode),
+    Pipe(GenericInode),
+    Socket(GenericInode)
+}
+
+struct GenericInode {
+    size: usize,
+    uid: usize,
+    gid: usize,
+    mode: usize,
+    linkcount: usize,
+    atime: u64,
+    ctime: u64,
+    mtime: u64,
+    refcount: usize
+}
+
+struct DirInode {
+    size: usize,
+    uid: usize,
+    gid: usize,
+    mode: usize,
+    linkcount: usize,
+    atime: u64,
+    ctime: u64,
+    mtime: u64
+    filename_to_inode_dict: RustHashMap<String, usize>,
+    refcount: usize
+}
+
+
 pub struct FilesystemMetadata {
     nextinode: usize,
     dev_id: usize,
-    inodetable: RustHashMap<usize, 
+    inodetable: RustHashMap<usize, Inode>
     fileobjecttable: RustHashMap<usize, EmulatedFile>
 } 
 
 impl FilesystemMetadata {
 
 }
-
