e7210d444ab2074b63a7f1d8d72ea50210a2ddf4
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2021-06-09 15:40:08 -0400

    filesystem

--- src/safeposix/dispatcher.rs
+++ src/safeposix/dispatcher.rs
@@ -16,9 +16,14 @@ pub union Arg {
 pub extern "C" fn dispatcher(callnum: i32, arg1: Arg, arg2: Arg, arg3: Arg, arg4: Arg, arg5: Arg, arg6: Arg) -> i32 {
     
     let cageid = interface::rust_gettid(); //figure this out
+
+    // need to match based on if cage exists
     let current_cage = cage_table[cageid];
 
     //implement syscall method calling using matching
+    // match based on syscall, take cagetable lock as read for all except fork/exec, which need write
+
+    match {} //blah blah
 
     return code;
 }
