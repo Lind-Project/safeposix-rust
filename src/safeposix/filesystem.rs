98be0cb4dfdb5f875d0920b916067786f96b7e24
Nicholas Renner <nr2185@nyu.edu>
2021-06-29 22:37:00 -0400

    Persistmetadata (#26)
    * serialize first attempt
    
    * serialize first attempt
    
    * serialize first attempt
    
    * compiles, needs testand pos refactor
    
    * pr fixes
    
    * Metadata now serializes reasonably! (#24)
    
    * new
    
    * new
    
    * reorganized persist/restore, PR changes
    
    Co-authored-by: jesings <34111484+jesings@users.noreply.github.com>

--- src/safeposix/filesystem.rs
+++ src/safeposix/filesystem.rs
@@ -5,15 +5,19 @@ use crate::interface;
 use super::syscalls::fs_constants::*;
 use super::cage::Cage;
 
+const METADATAFILENAME: &str = "lind.metadata";
+
 pub static FS_METADATA: interface::RustLazyGlobal<interface::RustRfc<interface::RustLock<FilesystemMetadata>>> = 
     interface::RustLazyGlobal::new(||
         interface::RustRfc::new(interface::RustLock::new(FilesystemMetadata::blank_fs_init()))
     ); //we want to check if fs exists before doing a blank init, but not for now
+
+
 type FileObjectTable = interface::RustHashMap<usize, interface::EmulatedFile>;
 pub static FILEOBJECTTABLE: interface::RustLazyGlobal<interface::RustLock<FileObjectTable>> = 
     interface::RustLazyGlobal::new(|| interface::RustLock::new(interface::RustHashMap::new()));
 
-#[derive(Debug)]
+#[derive(interface::SerdeSerialize, interface::SerdeDeserialize, Debug)]
 pub enum Inode {
     File(GenericInode),
     CharDev(DeviceInode),
@@ -23,7 +27,7 @@ pub enum Inode {
     Socket(GenericInode)
 }
 
-#[derive(Debug)]
+#[derive(interface::SerdeSerialize, interface::SerdeDeserialize, Debug)]
 pub struct GenericInode {
     pub size: usize,
     pub uid: u32,
@@ -35,7 +39,7 @@ pub struct GenericInode {
     pub ctime: u64,
     pub mtime: u64
 }
-#[derive(Debug)]
+#[derive(interface::SerdeSerialize, interface::SerdeDeserialize, Debug)]
 pub struct DeviceInode {
     pub size: usize,
     pub uid: u32,
@@ -49,7 +53,7 @@ pub struct DeviceInode {
     pub dev: DevNo,
 }
 
-#[derive(Debug)]
+#[derive(interface::SerdeSerialize, interface::SerdeDeserialize, Debug)]
 pub struct DirectoryInode {
     pub size: usize,
     pub uid: u32,
@@ -60,20 +64,20 @@ pub struct DirectoryInode {
     pub atime: u64,
     pub ctime: u64,
     pub mtime: u64,
-    pub filename_to_inode_dict: interface::RustHashMap<interface::OsStringKey, usize>
+    pub filename_to_inode_dict: interface::RustHashMap<String, usize>
 }
 
-
+#[derive(interface::SerdeSerialize, interface::SerdeDeserialize, Debug)]
 pub struct FilesystemMetadata {
     pub nextinode: usize,
     pub dev_id: u64,
     pub inodetable: interface::RustHashMap<usize, Inode>,
 }
 
-pub fn init_filename_to_inode_dict(parentinode: usize) -> interface::RustHashMap<interface::OsStringKey, usize> {
+pub fn init_filename_to_inode_dict(parentinode: usize) -> interface::RustHashMap<String, usize> {
     let mut retval = interface::RustHashMap::new();
-    retval.insert(interface::OsStringKey::from("."), ROOTDIRECTORYINODE);
-    retval.insert(interface::OsStringKey::from(".."), parentinode);
+    retval.insert(".".to_string(), ROOTDIRECTORYINODE);
+    retval.insert("..".to_string(), parentinode);
     retval
 }
 
@@ -87,15 +91,38 @@ impl FilesystemMetadata {
             atime: time, ctime: time, mtime: time,
             filename_to_inode_dict: init_filename_to_inode_dict(ROOTDIRECTORYINODE)};
         retval.inodetable.insert(ROOTDIRECTORYINODE, Inode::Dir(dirinode));
+
         retval
     }
 }
 
-pub fn load_fs_special_files() {
+pub fn load_fs() {
+
+    // Create initial cage, probably will move this
     let utilcage = Cage{cageid: 0,
-                        cwd: interface::RustLock::new(interface::RustRfc::new(interface::RustPathBuf::from("/"))),
-                        parent: 0, 
-                        filedescriptortable: interface::RustLock::new(interface::RustHashMap::new())};
+        cwd: interface::RustLock::new(interface::RustRfc::new(interface::RustPathBuf::from("/"))),
+        parent: 0, 
+        filedescriptortable: interface::RustLock::new(interface::RustHashMap::new())};
+
+    let metadata_fileobj = interface::openfile(METADATAFILENAME.to_string(), true);
+    
+    // If the metadata file exists, just close the file for later restore
+    // If it doesn't, lets create a new one and persist it.
+    match metadata_fileobj {
+        Ok(file) => {file.close().unwrap();}
+        Err(_e) => {persist_metadata(&FilesystemMetadata::blank_fs_init());}
+    };
+
+    // Restore metadata to global
+    let mut mutmetadata = FS_METADATA.write().unwrap();
+    restore_metadata(&mut mutmetadata);
+
+    load_fs_special_files(&utilcage);
+
+}
+
+pub fn load_fs_special_files(utilcage: &Cage) {
+
     if utilcage.mkdir_syscall("/dev", S_IRWXA) != 0 {
         interface::log_to_stderr("making /dev failed. Skipping");
     }
@@ -117,16 +144,37 @@ pub fn load_fs_special_files() {
     }
 }
 
-pub fn persist_metadata() {
+// Serialize Metadata Struct to JSON, write to file
+pub fn persist_metadata(metadata: &FilesystemMetadata) {
+  
+    // Serialize metadata to string
+    let metadatastring = interface::serde_serialize_to_string(&metadata).unwrap();
+    
+    // remove file if it exists, assigning it to nothing to avoid the compiler yelling about unused result
+    let _ = interface::removefile(METADATAFILENAME.to_string());
+
+    // write to file
+    let mut metadata_fileobj = interface::openfile(METADATAFILENAME.to_string(), true).unwrap();
+    metadata_fileobj.writefile_from_string(metadatastring, 0).unwrap();
+    metadata_fileobj.close().unwrap();
 }
 
-pub fn restore_metadata() {
+// Read file, and deserialize json to FS METADATA
+pub fn restore_metadata(metadata: &mut FilesystemMetadata) {
 
+    // Read JSON from file
+    let metadata_fileobj = interface::openfile(METADATAFILENAME.to_string(), true).unwrap();
+    let metadatastring = metadata_fileobj.readfile_to_new_string(0).unwrap();
+    metadata_fileobj.close().unwrap();
+
+    // Restore metadata
+    *metadata = interface::serde_deserialize_from_string(&metadatastring).unwrap();
 }
 
 pub fn convpath(cpath: &str) -> interface::RustPathBuf {
     interface::RustPathBuf::from(cpath)
 }
+
 //returns tuple consisting of inode number of file (if it exists), and inode number of parent (if it exists)
 pub fn metawalkandparent(path: &interface::RustPath, guard: Option<&FilesystemMetadata>) -> (Option<usize>, Option<usize>) {
     let ourreader;
@@ -154,7 +202,7 @@ pub fn metawalkandparent(path: &interface::RustPath, guard: Option<&FilesystemMe
                         previnodeno = inodeno;
 
                         //populate child inode number from parent directory's inode dict
-                        inodeno = match d.filename_to_inode_dict.get(f) {
+                        inodeno = match d.filename_to_inode_dict.get(&f.to_str().unwrap().to_string()) {
                             Some(num) => {
                                 curnode = md.inodetable.get(&num);
                                 Some(*num)
