37b1acde0e0f6414dcf48350717a615ddd06a803
jesings <34111484+jesings@users.noreply.github.com>
2021-07-22 15:02:05 -0400

    Added simple bench

--- src/tests/fs_tests.rs
+++ src/tests/fs_tests.rs
@@ -3,6 +3,7 @@ mod fs_tests {
     use crate::interface;
     use crate::safeposix::{cage::*, filesystem, dispatcher::*};
     use super::super::*;
+    extern crate test;
 
     #[test]
     pub fn test_fs() {
@@ -19,7 +20,45 @@ mod fs_tests {
         dispatch_tests::cagetest();
     }
 
+    #[bench]
+    fn rwtimer(b: &mut test::bench::Bencher) {
+        b.bench(|_| {
+        lindrustinit();
+        let cage = {CAGE_TABLE.read().unwrap().get(&1).unwrap().clone()};
 
+        let bcount: i32 = (128/16) * 1024;
+        let textstring = std::iter::repeat("1234567890ABCDEF").take((bcount / 16) as usize).collect::<String>();
+        let textstr = textstring.as_str();
+        let textbuf = str2cbuf(textstr);
+        let mut otherbuf = sizecbuf(bcount as usize);
+
+        let mut timerstart = interface::starttimer();
+        let fd = cage.open_syscall("16MBhex", O_CREAT | O_RDWR, S_IRWXA);
+        println!("{}ns for rwtimer to open", interface::readtimer(timerstart).as_nanos());
+        assert_ne!(fd, -1);
+
+        timerstart = interface::starttimer();
+        let writeres = cage.write_syscall(fd, textbuf, bcount as usize);
+        println!("{}ns for rwtimer to write", interface::readtimer(timerstart).as_nanos());
+        assert_eq!(writeres, bcount);
+
+        timerstart = interface::starttimer();
+        let lseekres = cage.lseek_syscall(fd, 0, SEEK_SET);
+        println!("{}ns for rwtimer to lseek", interface::readtimer(timerstart).as_nanos());
+        assert_eq!(lseekres, 0);
+
+        timerstart = interface::starttimer();
+        let readres = cage.read_syscall(fd, otherbuf.as_mut_ptr(), (bcount + 1) as usize);
+        println!("{}ns for rwtimer to read", interface::readtimer(timerstart).as_nanos());
+        assert_eq!(readres, bcount);
+        assert_eq!(cbuf2str(&otherbuf), textstr);
+
+        timerstart = interface::starttimer();
+        let exitres = cage.exit_syscall();
+        println!("{}ns for rwtimer to exit", interface::readtimer(timerstart).as_nanos());
+        lindrustfinalize();
+        });
+    }
 
     pub fn persistencetest() {
         lindrustinit();
