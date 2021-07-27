2cca0eaaa838d3e2eb4a56d800f7c3beeffdc17d
Tristan Brigham <tristan.brigham@sps.edu>
2021-07-27 17:11:20 +0300

    Updating the types

--- src/interface/types.rs
+++ src/interface/types.rs
@@ -22,27 +22,27 @@ pub fn get_uint(union_argument: Arg) -> Result<u32, i32> {
 }
 
 pub fn get_long(union_argument: Arg) -> Result<i64, i32> {
-    if let data = unsafe{union_argument.dispatch_long} { //this should not error out
+    if let data = unsafe{union_argument.dispatch_long} { //this should not return error 
         return Ok(data);
     }
     return Err(syscall_error(Errno::EINVAL, "dispatcher", "input data not valid"));
 }
 
 pub fn get_ulong(union_argument: Arg) -> Result<u64, i32> {
-    if let data = unsafe{union_argument.dispatch_ulong} {   //this should not error out
+    if let data = unsafe{union_argument.dispatch_ulong} {   //this should not return error 
         return Ok(data);
     }
     return Err(syscall_error(Errno::EINVAL, "dispatcher", "input data not valid"));
 }
 
-pub fn get_isize(union_argument: Arg) -> Result<isize, i32> {
+pub fn get_isize(union_argument: Arg) -> Result<isize, i32> { // also should not return error
     if let data = unsafe{union_argument.dispatch_isize} {
         return Ok(data);
     }
     return Err(syscall_error(Errno::EINVAL, "dispatcher", "input data not valid"));
 }
 
-pub fn get_usize(union_argument: Arg) -> Result<usize, i32> {
+pub fn get_usize(union_argument: Arg) -> Result<usize, i32> { //should not return an error
     if let data = unsafe{union_argument.dispatch_usize} {
         return Ok(data);
     }
