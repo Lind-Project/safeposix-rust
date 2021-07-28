ae5ac1a53e37637661f08074b535eb5ead876ea4
jesings <34111484+jesings@users.noreply.github.com>
2021-07-28 14:41:31 -0400

    Added epoll fds (#50)
    * Added epoll fds
    
    * Cleanup
    
    * Removed no longer relevant comment

--- src/safeposix/syscalls/net_constants.rs
+++ src/safeposix/syscalls/net_constants.rs
@@ -3,4 +3,13 @@
 use crate::interface;
 
 // Define constants using static or const
-// Imported into fs_calls file
+// Imported into net_calls file
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct EpollEvent {
+    pub events: u32,
+    pub fd: i32 
+    //in native this is a union which could be one of a number of things
+    //however, we only support EPOLL_CTL subcommands which take the fd
+}
