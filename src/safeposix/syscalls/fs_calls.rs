e4bc26866aab7dc3f31e21f4addda025e92f9df7
Tristan Brigham <tristan.brigham@sps.edu>
2021-08-06 09:12:01 -0400

    Fixing the issue that Nick found

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -1,7 +1,6 @@
 // File system related system calls
 use crate::interface;
-use crate::interface::types::{FSData, StatData, Arg};
-use crate::safeposix::cage::{CAGE_TABLE, Cage, FileDescriptor::*, FileDesc, FdTable, Errno, syscall_error};
+use crate::safeposix::cage::{Arg, Cage, CAGE_TABLE, FileDescriptor::*, FileDesc, FdTable, FSData, Errno, StatData, syscall_error};
 use crate::safeposix::filesystem::*;
 use super::fs_constants::*;
 
