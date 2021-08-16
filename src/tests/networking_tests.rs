53cb6259a9c7c8355ec15e45499c759996be97ba
Tristan Brigham <tristan.brigham@sps.edu>
2021-08-16 16:43:33 -0400

    Adding tests

--- src/tests/networking_tests.rs
+++ src/tests/networking_tests.rs
@@ -25,7 +25,7 @@ mod fs_tests {
         let sockfd = cage.socket_syscall(AF_INET, SOCK_STREAM, 0);
 
         //should work...
-        let socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50102, sin_addr: interface::V4Addr{ s_addr: 0b01111111000000000000000000000001 }}); //127.0.0.1
+        let socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50102, sin_addr: interface::V4Addr{ s_addr: u32::from_be_bytes([127, 0, 0, 1]) }}); //127.0.0.1
 
         assert_eq!(cage.bind_syscall(sockfd, &socket, 4096), 0);
         assert_eq!(cage.bind_syscall(sockfd, &socket, 4096), -(Errno::EINVAL as i32)); //already bound so should fail
@@ -54,7 +54,7 @@ mod fs_tests {
         let clientsockfd = cage.socket_syscall(AF_INET, SOCK_STREAM, 0);
         let clientsockfd2 = cage.socket_syscall(AF_INET, SOCK_STREAM, 0);
 
-        let socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50102, sin_addr: interface::V4Addr{ s_addr: 0 }}); //127.0.0.1
+        let socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50103, sin_addr: interface::V4Addr{ s_addr: 0 }}); //127.0.0.1
         assert_eq!(cage.bind_syscall(serversockfd, &socket, 4096), 0);
         assert_eq!(cage.listen_syscall(serversockfd, 1), 0);
 
@@ -69,14 +69,14 @@ mod fs_tests {
         let cage = {CAGE_TABLE.read().unwrap().get(&1).unwrap().clone()};
 
         let mut sockfd = cage.socket_syscall(AF_INET, SOCK_STREAM, 0);
-        let socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50102, sin_addr: interface::V4Addr{ s_addr: 0b01111111000000000000000000000001 }}); //127.0.0.1
+        let socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50104, sin_addr: interface::V4Addr{ s_addr: u32::from_be_bytes([127, 0, 0, 1]) }}); //127.0.0.1
         assert_eq!(cage.bind_syscall(sockfd, &socket, 4096), 0);
 
         let sockfd2 = cage.socket_syscall(AF_INET, SOCK_STREAM, 0);
 
         //allowing port reuse
-        assert_eq!(cage.setsockopt_syscall(sockfd, SOL_SOCKET, SO_REUSEPORT, 1), 1);
-        assert_eq!(cage.setsockopt_syscall(sockfd2, SOL_SOCKET, SO_REUSEPORT, 1), 1);
+        assert_eq!(cage.setsockopt_syscall(sockfd, SOL_SOCKET, SO_REUSEPORT, 1), 0);
+        assert_eq!(cage.setsockopt_syscall(sockfd2, SOL_SOCKET, SO_REUSEPORT, 1), 0);
 
         assert_eq!(cage.bind_syscall(sockfd2, &socket, 4096), 0);
 
@@ -100,11 +100,11 @@ mod fs_tests {
 
         //should be okay...
         let sockfd = cage.socket_syscall(AF_INET, SOCK_DGRAM, 0);
-        let mut socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50103, sin_addr: interface::V4Addr{ s_addr: 0b01111111000000000000000000000001 }}); //127.0.0.1
+        let mut socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50105, sin_addr: interface::V4Addr{ s_addr: u32::from_be_bytes([127, 0, 0, 1]) }}); //127.0.0.1
         assert_eq!(cage.connect_syscall(sockfd, &socket), 0);
 
         //should be able to retarget the socket
-        socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50104, sin_addr: interface::V4Addr{ s_addr: 0b01111111000000000000000000000001 }}); //127.0.0.1
+        socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50106, sin_addr: interface::V4Addr{ s_addr: u32::from_be_bytes([127, 0, 0, 1]) }}); //127.0.0.1
         assert_eq!(cage.connect_syscall(sockfd, &socket), 0);
 
         assert_eq!(cage.exit_syscall(), 0);
@@ -119,7 +119,7 @@ mod fs_tests {
 
         //doing a few things with connect -- only UDP right now
         let sockfd = cage.socket_syscall(AF_INET, SOCK_DGRAM, 0);
-        let mut socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50103, sin_addr: interface::V4Addr{ s_addr: 0b01111111000000000000000000000001 }}); //127.0.0.1
+        let mut socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50107, sin_addr: interface::V4Addr{ s_addr: u32::from_be_bytes([127, 0, 0, 1]) }}); //127.0.0.1
         let mut retsocket = interface::GenSockaddr::V4(interface::SockaddrV4::default()); //127.0.0.1
         
         assert_eq!(cage.connect_syscall(sockfd, &socket), 0);
@@ -127,8 +127,7 @@ mod fs_tests {
         assert_eq!(retsocket, socket);
 
         //should be able to retarget
-        socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50104, sin_addr: interface::V4Addr{ s_addr: 0b01111111000000000000000000000001 }}); //127.0.0.1
-        
+        socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50108, sin_addr: interface::V4Addr{ s_addr: u32::from_be_bytes([127, 0, 0, 1]) }}); //127.0.0.1
         assert_eq!(cage.connect_syscall(sockfd, &socket), 0);
         assert_eq!(cage.getpeername_syscall(sockfd, &mut retsocket), 0);
         assert_eq!(retsocket, socket);
@@ -144,22 +143,23 @@ mod fs_tests {
         let cage = {CAGE_TABLE.read().unwrap().get(&1).unwrap().clone()};
         
         let sockfd = cage.socket_syscall(AF_INET, SOCK_STREAM, 0);
-        let mut retsocket = interface::GenSockaddr::V4(interface::SockaddrV4::default()); //127.0.0.1
+        let mut retsocket = interface::GenSockaddr::V4(interface::SockaddrV4::default()); 
 
         assert_eq!(cage.getsockname_syscall(sockfd, &mut retsocket), 0);
         assert_eq!(retsocket.port(), 0);
         assert_eq!(retsocket.addr(), interface::GenIpaddr::V4(interface::V4Addr::default()));
 
-        let mut socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50102, sin_addr: interface::V4Addr{ s_addr: 0b01111111000000000000000000000001 }}); //127.0.0.1
+        let mut socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50109, sin_addr: interface::V4Addr{ s_addr: u32::from_be_bytes([127, 0, 0, 1]) }}); //127.0.0.1
         
         assert_eq!(cage.bind_syscall(sockfd, &socket, 4096), 0);
         assert_eq!(cage.getsockname_syscall(sockfd, &mut retsocket), 0);
         assert_eq!(retsocket, socket);    
 
-        socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50103, sin_addr: interface::V4Addr{ s_addr: 0b01111111000000000000000000000001 }}); //127.0.0.1
+        //checking that we cannot rebind the socket
+        socket = interface::GenSockaddr::V4(interface::SockaddrV4{ sin_family: 0, sin_port: 50110, sin_addr: interface::V4Addr{ s_addr: u32::from_be_bytes([127, 0, 0, 1]) }}); //127.0.0.1
         assert_eq!(cage.bind_syscall(sockfd, &socket, 4096), -(Errno::EINVAL as i32)); //already bound so should fail
         assert_eq!(cage.getsockname_syscall(sockfd, &mut retsocket), 0);
-        assert_eq!(retsocket, socket);
+        assert_ne!(retsocket, socket);
 
         assert_eq!(cage.exit_syscall(), 0);
         lindrustfinalize();
