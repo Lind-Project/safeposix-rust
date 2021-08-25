7d5d1b4807e36d4bbfc3df63e8e82f85641a17a9
Tristan Brigham <tristan.brigham@sps.edu>
2021-08-25 15:23:38 -0700

    adding constant

--- src/safeposix/syscalls/net_calls.rs
+++ src/safeposix/syscalls/net_calls.rs
@@ -12,6 +12,8 @@ use crate::safeposix::cage::{CAGE_TABLE, Cage, FileDescriptor::*, SocketDesc, Ep
 use crate::safeposix::filesystem::*;
 use crate::safeposix::net::*;
 
+pub const BLOCK_TIME: interface::RustDuration = interface::RustDuration::from_micros(100);
+
 impl Cage {
     fn _socket_initializer(&self, domain: i32, socktype: i32, protocol: i32, blocking: bool, cloexec: bool) -> SocketDesc {
         let flags = if blocking {O_NONBLOCK} else {0} | if cloexec {O_CLOEXEC} else {0};
