60d2d7623c07e2b54c8ac862ced1ae82201f5489
Tristan Brigham <tristan.brigham@sps.edu>
2021-09-08 09:09:26 -0400

    Stashing Changes

--- src/tests/networking_tests.rs
+++ src/tests/networking_tests.rs
@@ -782,8 +782,9 @@ pub mod net_tests {
             interface::sleep(interface::RustDuration::from_millis(100)); 
 
             let mut buf = sizecbuf(16);
-            assert_eq!(cage2.recvfrom_syscall(listenfd, buf.as_mut_ptr(), 16, 0, &mut Some(&mut socket_clone)), 16);
             panic!();
+
+            assert_eq!(cage2.recvfrom_syscall(listenfd, buf.as_mut_ptr(), 16, 0, &mut Some(&mut socket_clone)), 16);
             assert_eq!(cbuf2str(&buf), "UDP Connect Test");
 
             assert_eq!(cage2.close_syscall(listenfd), 0);
