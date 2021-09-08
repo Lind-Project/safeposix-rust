98b486ddc10da02f698d0eedaf3a64bf45436da7
Tristan Brigham <tristan.brigham@sps.edu>
2021-09-08 09:09:02 -0400

    Stashing Changes

--- src/tests/networking_tests.rs
+++ src/tests/networking_tests.rs
@@ -6,7 +6,6 @@ pub mod net_tests {
     use std::mem::size_of;
 
     pub fn net_tests() {
-        println!("NET TESTS");
         ut_lind_net_bind();
         ut_lind_net_bind_multiple();
         ut_lind_net_bind_on_zero(); 
@@ -783,9 +782,8 @@ pub mod net_tests {
             interface::sleep(interface::RustDuration::from_millis(100)); 
 
             let mut buf = sizecbuf(16);
-            println!("RECV");
             assert_eq!(cage2.recvfrom_syscall(listenfd, buf.as_mut_ptr(), 16, 0, &mut Some(&mut socket_clone)), 16);
-            println!("RECV'd");
+            panic!();
             assert_eq!(cbuf2str(&buf), "UDP Connect Test");
 
             assert_eq!(cage2.close_syscall(listenfd), 0);
