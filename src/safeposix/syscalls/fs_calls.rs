60660e15a5775e6a9658ccc8d3ea307ca39b0224
Çağlar Doğan <dogancaglar05@gmail.com>
2021-12-28 22:37:50 -0500

    Added getcwd syscall

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -1727,3 +1727,17 @@ impl Cage {
         }
     }
 }
+
+//------------------------------------GETCWD SYSCALL------------------------------------
+    
+pub fn getcwd_syscall(&self, buf: *mut u8, bufsize: u32) -> i32 {
+    let cwd_container = self.cwd.write().unwrap();
+
+    //+1 foor null terminator
+    if bufsize < cwd_container.len() + 1 {
+        return syscall_error(Errno::ERANGE, "getcwd", "the length (in bytes) of the absolute pathname of the current working directory exceeds the given size");
+    }
+
+    *buf = self.cwd.write().unwrap();
+    0 //getcwd has succeeded!;
+}
