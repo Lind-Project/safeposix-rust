f63c93ccd175b50149cd585e3e9d0e9731088b6c
Nicholas Renner <nicholassrenner@gmail.com>
2022-01-12 22:45:45 -0500

    adv lock table

--- src/safeposix/cage.rs
+++ src/safeposix/cage.rs
@@ -16,7 +16,7 @@ pub static PIPE_TABLE: interface::RustLazyGlobal<interface::RustLock<interface::
         interface::RustLock::new(interface::new_hashmap())
 );
 
-pub static CAGE_TABLE: interface::RustLazyGlobal<interface::RustLock<interface::RustHashMap<u64, interface::RustRfc<AdvisoryLock>>>> = interface::RustLazyGlobal::new(|| interface::RustLock::new(interface::new_hashmap()));
+pub static LOCK_TABLE: interface::RustLazyGlobal<interface::RustLock<interface::RustHashMap<u64, interface::RustRfc<AdvisoryLock>>>> = interface::RustLazyGlobal::new(|| interface::RustLock::new(interface::new_hashmap()));
 
 
 #[derive(Debug, Clone)]
@@ -156,7 +156,7 @@ pub fn get_next_pipe() -> Option<i32> {
 
 pub fn add_advlock() -> Option<u64> {
     let table = LOCK_TABLE.write().unwrap();
-    for fd in STARTINGPIPE..MAXPIPE {
+    for fd in 0..10000 {
         if !table.contains_key(&fd) {
             table.insert(interface::RustRfc::new(interface::AdvisoryLock::new()))
             return Some(fd);
