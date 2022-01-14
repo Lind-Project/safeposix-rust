0fe5ab2119273fc2a719075d2cc10851f911d75e
Nicholas Renner <nicholassrenner@gmail.com>
2022-01-13 19:41:16 -0500

    no rfc

--- src/safeposix/cage.rs
+++ src/safeposix/cage.rs
@@ -158,8 +158,6 @@ pub fn add_advlock() -> Option<u64> {
     let mut table = LOCK_TABLE.write().unwrap();
     for locknum in 0..10000 {
         if !table.contains_key(&locknum) {
-            println!("new adv{:?}", locknum);
-
             table.insert(locknum, interface::AdvisoryLock::new());
             return Some(locknum);
         }
