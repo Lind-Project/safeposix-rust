fc67f42e8ef7c2404a90a8880186ae40f0d4b6d6
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-03 23:54:26 -0500

    first draft metadata logging

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -69,7 +69,7 @@ impl Cage {
                     ind.filename_to_inode_dict.insert(filename, newinodenum);
                     ind.linkcount += 1;
                 } //insert a reference to the file in the parent directory
-                log_metadata(&mutmetadata, newinodenum, Some(newinode));
+                log_metadata(&mutmetadata, newinodenum, Some(newinode.as_ref()));
                 mutmetadata.inodetable.insert(newinodenum, newinode);
             }
 
