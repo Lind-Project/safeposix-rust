6da2c30915675f1cf70e7e289a3271cc7b4e0160
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-03 22:10:09 -0500

    first draft metadata logging

--- src/safeposix/filesystem.rs
+++ src/safeposix/filesystem.rs
@@ -198,7 +198,7 @@ pub fn log_metadata(metadata: &FilesystemMetadata, newinodenum: usize, newinode:
   
     // pack and serialize log entry
     let entry = LogEntry{inodenum: newinodenum, inode: newinode};
-    let entrystring = interface::serde_serialize_to_string(&entry).unwrap();//.push('\n');
+    let mut entrystring = interface::serde_serialize_to_string(&entry).unwrap().push('\n');
 
     // write to file
     let mut metadata_fileobj = &metadata.logfile.unwrap();
