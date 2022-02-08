1cf0ab2f191f6203d77f158159d671d73dc51711
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-08 18:15:40 -0500

    switch to bincode

--- src/safeposix/filesystem.rs
+++ src/safeposix/filesystem.rs
@@ -136,7 +136,7 @@ pub fn load_fs() {
             let logbytes = log_fileobj.readfile_to_new_bytes().unwrap();
             let mut logvec: Vec<Vec<u8>> = logbytes.split('\n').collect();
             for logline in logvec.iter_mut() {
-                let serialpair: (usize, Option<Inode>) = interface::serde_deserialize_from_bytes(&logline.as_bytes()).unwrap();
+                let serialpair: (usize, Option<Inode>) = interface::serde_deserialize_from_bytes(&logline).unwrap();
                 let (inodenum, inode) = serialpair;
                 match inode {
                     Some(inode) => mutmetadata.inodetable.insert(inodenum, inode),
