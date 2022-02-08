e5fcdda95227e5d20821f8e5acfc6d5f2f9ad039
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-08 17:46:46 -0500

    switch to bincode

--- src/safeposix/filesystem.rs
+++ src/safeposix/filesystem.rs
@@ -204,7 +204,7 @@ pub fn log_metadata(metadata: &FilesystemMetadata, inodenum: usize) {
     let serialpair: (usize, Option<&Inode>) = (inodenum, inode);
 
     let mut entrystring = interface::serde_serialize_to_bytes(&serialpair).unwrap();
-    entrystring.push('\n');
+    entrystring.push(b'\n');
 
     // write to file
     LOGFILE.get().unwrap().write().unwrap().writefile_from_bytes(entrystring).unwrap();
