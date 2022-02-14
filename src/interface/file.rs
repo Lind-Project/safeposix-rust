0284b395584269ee0f6c8b249da1a404998d588c
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-14 18:30:02 -0500

    adding mmap file to cbor

--- src/interface/file.rs
+++ src/interface/file.rs
@@ -371,7 +371,7 @@ impl EmulatedFileMap {
 
     fn increase_map(&mut self) {
 
-        let mut map = self.map.lock().unwrap().unwrap();
+        let &mut map = self.map.lock().unwrap().unwrap();
         let f = self.fobj.lock().unwrap();
 
         let new_mapsize = self.mapsize + usize::pow(2, 20);
