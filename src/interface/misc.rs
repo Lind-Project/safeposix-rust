1b8a9b559d9f59a61f4bc11e42fc058b6bcdf07d
Tristan Brigham <tristan.brigham@sps.edu>
2022-02-20 17:15:32 -0500

    Small semantic change

--- src/interface/misc.rs
+++ src/interface/misc.rs
@@ -6,8 +6,8 @@
 
 use std::fs::File;
 use std::io::{self, Read, Write};
-pub use dashmap::{DashSet as RustHashSet, DashMap as mmmmmap};
-pub use std::collections::{HashMap as RustHashMap, VecDeque as RustDeque};
+pub use dashmap::{DashSet as RustHashSet, DashMap as RustHashMap};
+pub use std::collections::{VecDeque as RustDeque};
 pub use std::cmp::{max as rust_max, min as rust_min};
 pub use std::sync::atomic::{AtomicBool as RustAtomicBool, Ordering as RustAtomicOrdering, AtomicU16 as RustAtomicU16, AtomicI32 as RustAtomicI32};
 pub use std::thread::spawn as helper_thread;
