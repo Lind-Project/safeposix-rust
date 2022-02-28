d98436d6e4c5922d41ca06158c3f55dd6740295f
Tristan Brigham <tristan.brigham@sps.edu>
2022-02-28 10:17:29 -0500

    Small Change

--- src/safeposix/syscalls/sys_calls.rs
+++ src/safeposix/syscalls/sys_calls.rs
@@ -106,8 +106,8 @@ impl Cage {
         //close all remaining files in the fdtable
         {
             let mut fdtable = self.filedescriptortable;
-            let files2close = fdtable.iter().map(|x| *x).collect::<Vec<i32>>();
-            for fd in files2close {
+            // let files2close = fdtable.iter().map(|x| *x).collect::<Vec<i32>>();
+            for (fd, _) in fdtable.iter() {
                 self._close_helper(fd, Some(&mut fdtable));
             }
         }
