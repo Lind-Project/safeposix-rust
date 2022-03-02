824401bcef73631fa4b460bcb1f5a7af4d9b44b9
Caglar Dogan <dogancaglar05@gmail.com>
2022-03-01 21:39:58 -0500

    Update

--- src/safeposix/syscalls/fs_constants.rs
+++ src/safeposix/syscalls/fs_constants.rs
@@ -79,6 +79,7 @@ pub const F_NOTIFY: i32 = 1026;
 
 //Commands for IOCTL
 pub const FIONBIO: u32 = 21537;
+pub const FIOASYNC: u32 = 21536; //not implemented
 
 //File types for open/stat etc.
 pub const S_IFBLK: i32 = 0o60000;
