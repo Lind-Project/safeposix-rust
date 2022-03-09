28113d48e81a481cfe4d9ca461367c44c8ad5e5e
Tristan Brigham <tristan.brigham@sps.edu>
2022-03-09 12:33:05 -0700

    small changes

--- src/safeposix/syscalls/net_calls.rs
+++ src/safeposix/syscalls/net_calls.rs
@@ -1345,7 +1345,7 @@ impl Cage {
 
                 let mut poll_fds_vec: Vec<PollStruct> = vec![];
 
-                for (key, value) in epollfdobj.registered_fds.iter() {
+                for (&key, &value) in epollfdobj.registered_fds.iter().unwrap() {
 
                     let events = value.events;
                     let mut structpoll = PollStruct {
