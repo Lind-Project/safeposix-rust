968d0abd2189174f3d424242149e38d83a498ad8
Tristan Brigham <tristan.brigham@sps.edu>
2022-03-09 14:30:30 -0700

    small changes

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -64,7 +64,7 @@ impl Cage {
                 });
 
                 let newinodenum = mutmetadata.nextinode;
-                *mutmetadata.nextinode += 1;
+                mutmetadata.nextinode += 1;
                 if let Inode::Dir(ref mut ind) = *mutmetadata.inodetable.get_mut(&pardirinode).unwrap() {
                     ind.filename_to_inode_dict.insert(filename, newinodenum);
                     ind.linkcount += 1;
