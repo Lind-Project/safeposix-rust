89a0548f9433cce3150263e5a7cb817d58d883ce
Tristan Brigham <tristan.brigham@sps.edu>
2022-03-09 14:25:58 -0700

    small changes

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -337,7 +337,7 @@ impl Cage {
                 }; //count current number of links and references
 
                 let parentinodeobj = mutmetadata.inodetable.get_mut(&parentinodenum).unwrap();
-                let &mut directory_parent_inode_obj = if let Inode::Dir(mut ref x) = *parentinodeobj {x} else {
+                let &mut directory_parent_inode_obj = if let Inode::Dir(ref mut x) = *parentinodeobj {x} else {
                     panic!("File was a child of something other than a directory????");
                 };
                 directory_parent_inode_obj.filename_to_inode_dict.remove(&truepath.file_name().unwrap().to_str().unwrap().to_string()); //for now we assume this is sane, but maybe this should be checked later
