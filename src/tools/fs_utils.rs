db585dfeeb26690ddcdb5d6914fc8cd8f9d2b0e5
Tristan Brigham <tristan.brigham@sps.edu>
2022-03-10 13:53:37 -0700

    small changes

--- src/tools/fs_utils.rs
+++ src/tools/fs_utils.rs
@@ -158,7 +158,7 @@ fn main() {
 
         "format" => {
             lind_deltree(&utilcage, "/"); //This doesn't actually fully remove all of the linddata files... TODO: debug
-            FS_METADATA = FilesystemMetadata::blank_fs_init();
+            FS_METADATA = interface::RustLazyGlobal::new(|| interface::RustRfc::new(FilesystemMetadata::blank_fs_init()));
             drop(metadata);
             load_fs_special_files(&utilcage);
         }
