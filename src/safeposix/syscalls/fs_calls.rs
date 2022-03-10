38111b862f26efe2a06155037a9e3cfd09b44d75
Tristan Brigham <tristan.brigham@sps.edu>
2022-03-09 17:47:10 -0700

    small changes

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -225,7 +225,7 @@ impl Cage {
                     atime: time, ctime: time, mtime: time, dev: devtuple(dev)
                 });
 
-                let newinodenum = mutmetadata.nextinode.load(Ordering::Relaxed);
+                let newinodenum = mutmetadata.nextinode.load(interface::RustAtomicOrdering::Relaxed);
                 mutmetadata.nextinode.store(newinodenum + 1 as usize, interface::RustAtomicOrdering::Relaxed);
                 if let Inode::Dir(ref mut parentdir) = *mutmetadata.inodetable.get_mut(&pardirinode).unwrap() {
                     parentdir.filename_to_inode_dict.insert(filename, newinodenum);
