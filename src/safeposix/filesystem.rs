175a527bd2abc0e4ef672cb787019858f23267ca
Tristan Brigham <tristan.brigham@sps.edu>
2022-03-11 12:44:55 -0700

    file system tests

--- src/safeposix/filesystem.rs
+++ src/safeposix/filesystem.rs
@@ -109,20 +109,16 @@ impl FilesystemMetadata {
     // Read file, and deserialize CBOR to FS METADATA
     pub fn init_fs_metadata() -> FilesystemMetadata {
         // Read CBOR from file
-        let metadata_fileobj = interface::openfile(METADATAFILENAME.to_string(), false).unwrap();
-        let metadatabytes = metadata_fileobj.readfile_to_new_bytes().unwrap();
-        metadata_fileobj.close().unwrap();
-
-        
-        match interface::serde_deserialize_from_bytes(&metadatabytes) {
-            Ok(()) => {
-                interface::serde_deserialize_from_bytes(&metadatabytes).unwrap()
-            }
-            Err(_) => {
-                FilesystemMetadata::blank_fs_init()
-            }
+        if pathexists(METADATAFILENAME.to_string()) {
+            let metadata_fileobj = interface::openfile(METADATAFILENAME.to_string(), false).unwrap();
+            let metadatabytes = metadata_fileobj.readfile_to_new_bytes().unwrap();
+            metadata_fileobj.close().unwrap();
+    
+            // Restore metadata
+            interface::serde_deserialize_from_bytes(&metadatabytes).unwrap()
+        } else {
+            FilesystemMetadata::blank_fs_init()
         }
-        // Restore metadata
     }
 }
 
