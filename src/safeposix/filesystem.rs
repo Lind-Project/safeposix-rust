a7aa4f9cc4970fe186d3dbcaa70daf4dc848421c
Tristan Brigham <tristan.brigham@sps.edu>
2022-03-11 12:33:36 -0700

    file system tests

--- src/safeposix/filesystem.rs
+++ src/safeposix/filesystem.rs
@@ -113,6 +113,8 @@ impl FilesystemMetadata {
         let metadatabytes = metadata_fileobj.readfile_to_new_bytes().unwrap();
         metadata_fileobj.close().unwrap();
 
+        println!("wtf");
+
         match interface::serde_deserialize_from_bytes(&metadatabytes) {
             Ok(()) => {
                 interface::serde_deserialize_from_bytes(&metadatabytes).unwrap()
