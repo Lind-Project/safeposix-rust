2f31da86d6f4290d2a518793c2dc6bca9952f124
Nicholas Renner <nicholassrenner@gmail.com>
2022-03-11 16:06:45 -0500

    unset cloexec in dups

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -1137,7 +1137,21 @@ impl Cage {
             let locked_oldfiledesc = fdtable.get(&oldfd).unwrap();
             let oldfiledesc_enum = locked_oldfiledesc.read().unwrap();
             filedesc_clone = (*&oldfiledesc_enum).clone();
-            filedesc_clone.flags = filedesc_clone.flags & ~O_CLOEXEC; // we don't want to pass on the CLOEXEC flag
+            match &*filedesc_clone { // we don't want to pass on the CLOEXEC flag
+                File(normalfile_filedesc_obj) => {
+                    normalfile_filedesc_obj.flags = normalfile_filedesc_obj.flags & !O_CLOEXEC; 
+                }
+                Pipe(pipe_filedesc_obj) => {
+                    pipe_filedesc_obj.flags = pipe_filedesc_obj.flags & !O_CLOEXEC;
+                }
+                Socket(socket_filedesc_obj) => {
+                    socket_filedesc_obj.flags = socket_filedesc_obj.flags & !O_CLOEXEC;
+                }
+                Stream(stream_filedesc_obj) => {
+                    stream_filedesc_obj.flags = stream_filedesc_obj.flags & !O_CLOEXEC;
+                }
+            }
+
         }
 
         let wrappedfd = interface::RustRfc::new(interface::RustLock::new(filedesc_clone));
