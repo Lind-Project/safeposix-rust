d1fc93013a3cde32142620f2201044c38f812b6a
Tristan Brigham <tristan.brigham@sps.edu>
2022-03-11 12:31:11 -0700

    metadata fix

--- src/safeposix/filesystem.rs
+++ src/safeposix/filesystem.rs
@@ -109,20 +109,19 @@ impl FilesystemMetadata {
     // Read file, and deserialize CBOR to FS METADATA
     pub fn init_fs_metadata() -> FilesystemMetadata {
         // Read CBOR from file
-        if let metadata_fileobj = interface::openfile(METADATAFILENAME.to_string(), true).unwrap() {
-            let metadatabytes = metadata_fileobj.readfile_to_new_bytes().unwrap();
-            metadata_fileobj.close().unwrap();
+        let metadata_fileobj = interface::openfile(METADATAFILENAME.to_string(), true).unwrap() 
+        let metadatabytes = metadata_fileobj.readfile_to_new_bytes().unwrap();
+        metadata_fileobj.close().unwrap();
 
-            match interface::serde_deserialize_from_bytes(&metadatabytes) {
-                Ok(()) => {
-                    interface::serde_deserialize_from_bytes(&metadatabytes).unwrap()
-                }
-                Err(_) => {
-                    FilesystemMetadata::blank_fs_init()
-                }
+        match interface::serde_deserialize_from_bytes(&metadatabytes) {
+            Ok(()) => {
+                interface::serde_deserialize_from_bytes(&metadatabytes).unwrap()
             }
-            // Restore metadata
-        } 
+            Err(_) => {
+                FilesystemMetadata::blank_fs_init()
+            }
+        }
+        // Restore metadata
     }
 }
 
