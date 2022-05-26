065e919dd382dcf93324c21c884c005d8998dea4
Nicholas Renner <nicholassrenner@gmail.com>
2022-05-26 17:14:06 -0400

    bind use entry interface

--- src/safeposix/syscalls/net_calls.rs
+++ src/safeposix/syscalls/net_calls.rs
@@ -132,6 +132,9 @@ impl Cage {
         let intent_to_rebind = sockfdobj.options & (1 << SO_REUSEPORT) != 0;
         let mut newsockaddr = localaddr.clone();
         let mut pathopt = None;
+        let mut optinodenum = None;
+        let mut newinode = None;
+        let mut unixentry = None;
 
         if sockfdobj.realdomain == AF_UNIX {
             let path = localaddr.path();
@@ -153,11 +156,11 @@ impl Cage {
                     let effective_mode = S_IFSOCK as u32 | mode;
     
                     let time = interface::timestamp(); //We do a real timestamp now
-                    let newinode = Inode::Socket(SocketInode {
+                    newinode = Some(Inode::Socket(SocketInode {
                         size: 0, uid: DEFAULT_UID, gid: DEFAULT_GID,
                         mode: effective_mode, linkcount: 1, refcount: 1,
                         atime: time, ctime: time, mtime: time,
-                    });
+                    }));
     
                     let newinodenum = FS_METADATA.nextinode.fetch_add(1, interface::RustAtomicOrdering::Relaxed); //fetch_add returns the previous value, which is the inode number we want
                     println!("bind: found socket inode {:?} refcount = 1", newinodenum);
@@ -166,10 +169,17 @@ impl Cage {
                         ind.filename_to_inode_dict.insert(filename, newinodenum);
                         ind.linkcount += 1;
                     } //insert a reference to the file in the parent directory
-                    FS_METADATA.inodetable.insert(newinodenum, newinode);
+
+                    match FS_METADATA.inodetable.entry(newinodenum) {
+                        interface::RustHashEntry::Occupied(_) => {}
+                        interface::RustHashEntry::Vacant(vacant) => {
+                            unixentry = Some(vacant);
+                        }
+                    };
+
                     pathopt = Some(truepath.clone());
                     sockfdobj.reallocalpath = Some(truepath);
-                    sockfdobj.optinode = Some(newinodenum.clone());
+                    optinodenum = Some(newinodenum.clone());
 
                     // create fake IPV4 addr
                     let ipaddr = interface::V4Addr {s_addr: u32::from_ne_bytes([127, 0, 0, 1])};
@@ -210,6 +220,8 @@ impl Cage {
 
         sockfdobj.localaddr = Some(newsockaddr);
         if let Some(localpath) = pathopt {
+            unixentry.unwrap().insert(newinode.unwrap());
+            sockfdobj.optinode = optinodenum;
             NET_METADATA.domain_socket_table.insert(localpath, newsockaddr.clone());
             NET_METADATA.revds_table.insert(newsockaddr, localaddr.clone());
         }
