addf26f2810882f1034ff4783f071a137d68f19b
Nicholas Renner <nicholassrenner@gmail.com>
2022-08-23 17:05:44 -0400

    fix verbiage

--- src/interface/pipe.rs
+++ src/interface/pipe.rs
@@ -106,7 +106,7 @@ impl EmulatedPipe {
 
         while bytes_read < length {
             pipe_space = read_end.len();
-            if (pipe_space == 0) && self.eof.load(Ordering::Relaxed) { break; }
+            if (pipe_space == 0) && self.eof.load(Ordering::SeqCst) { break; }
             let bytes_to_read = min(length, bytes_read + pipe_space);
             read_end.pop_slice(&mut buf[bytes_read..bytes_to_read]);
             bytes_read = bytes_to_read;
