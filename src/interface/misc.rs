19930ef7e1cae689b1c3db985a3f38eb47a20d13
Nicholas Renner <nicholassrenner@gmail.com>
2023-01-26 23:20:33 -0500

    refactor cancellation

--- src/interface/misc.rs
+++ src/interface/misc.rs
@@ -104,7 +104,7 @@ pub fn get_pthreadid() {
 
 pub fn check_thread(cageid: u64, tid: u64, inrustposix: bool) -> bool {
     let cage = cagetable_getref(cageid);
-    let pthread_id = if inrustposix { get_pthreadid(); } } else { tid };
+    let pthread_id = if inrustposix { get_pthreadid(); } else { tid };
     let killable = *cage.thread_table.get(&pthread_id).unwrap();
     if inrustposix { cage.thread_table.insert(pthread_id, false); }
     killable
