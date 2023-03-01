1c2ee49d11e50be456e1f078cc32705a3d9dc162
Nicholas Renner <nicholassrenner@gmail.com>
2023-03-01 00:34:59 -0500

    quick io

--- src/safeposix/dispatcher.rs
+++ src/safeposix/dispatcher.rs
@@ -129,14 +129,12 @@ macro_rules! check_and_dispatch_socketpair {
 
 #[no_mangle]
 pub extern "C" fn quick_write(fd: i32, buf: *const u8, count: usize, cageid: u64) -> i32 {
-    let cage = interface::cagetable_getref(cageid);
-    cage.write_syscall(fd, buf, count)
+  unsafe { CAGE_TABLE[cageid as usize].as_ref().unwrap().write_syscall(fd, buf, count) }
 }
 
 #[no_mangle]
 pub extern "C" fn quick_read(fd: i32, buf: *mut u8, size: usize, cageid: u64) -> i32 {
-    let cage = interface::cagetable_getref(cageid);
-    cage.read_syscall(fd, buf, size)
+    unsafe { CAGE_TABLE[cageid as usize].as_ref().unwrap().read_syscall(fd, buf, size) }
 }
 
 #[no_mangle]
