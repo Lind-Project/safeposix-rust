f2dbd0ba838aa5fc7355ffd9d0f2db257578bef9
Tian(Maxwell) Yang <maxwell_yang@outlook.com>
2023-05-03 18:45:29 +0000

    Fixes

--- src/interface/timer.rs
+++ src/interface/timer.rs
@@ -9,6 +9,8 @@ use std::sync::Mutex;
 pub use std::time::Instant as RustInstant;
 pub use std::time::Duration as RustDuration;
 
+use super::lind_kill;
+
 pub fn timestamp() -> u64 {
     SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
 }
@@ -40,51 +42,54 @@ pub struct AlarmClock {
 }
 
 impl AlarmClock {
-    pub new() -> Self {
-        _ac: Mutex::new(
-            _AlarmClock {
-                cageid: None,
-                start_instant: RustInstant::now(),
-                duration: RustDuration::ZERO,
-                is_ticking: false,
-            }
-        )
+    pub fn new() -> Self {
+        Self {
+            _ac: Mutex::new(
+                _AlarmClock {
+                    cageid: None,
+                    start_instant: RustInstant::now(),
+                    duration: RustDuration::ZERO,
+                    is_ticking: false,
+                }
+            )
+        }
     }
 
-    pub lind_alarm(&self, seconds: u32, cageid: u64) -> u32 {
+    pub fn lind_alarm(&self, seconds: u32, cageid: u64) -> u32 {
         let mut guard = self._ac.lock().unwrap();
         let remaining_seconds = 0;
 
-        if *guard.is_ticking {
-            remaining_seconds = *guard.start_instant.elapsed().saturating_sub(*guard.duration).as_secs() as u32;
+        if guard.is_ticking {
+            remaining_seconds = guard.duration.saturating_sub(guard.start_instant.elapsed()).as_secs() as u32;
         }
 
-        *guard.cageid = Some(cageid);
-        *guard.start_instant = RustInstant::now();
-        *guard.duration = RustDuration::from_secs(seconds as u64);
-        *guard.is_ticking = true;
+        guard.cageid = Some(cageid);
+        guard.start_instant = RustInstant::now();
+        guard.duration = RustDuration::from_secs(seconds as u64);
+        guard.is_ticking = true;
 
         Mutex::unlock(guard);
+        remaining_seconds
     }
 
-    pub tick(&self) {
+    pub fn tick(&self) {
         while true {
             let mut guard = self._ac.lock().unwrap();
 
-            if *guard.is_ticking {
-                let remaining_seconds = *guard.start_instant.elapsed().checked_sub(*guard.duration);
+            if guard.is_ticking {
+                let remaining_seconds = guard.duration.checked_sub(guard.start_instant.elapsed());
 
                 match remaining_seconds {
                     Some(_) => (),
                     None    => {
-                        if let Some(cageid) = *guard.cageid {
-                            interface::lind_kill(cageid, 14);
-                            *guard.cageid = None;
+                        if let Some(cageid) = guard.cageid {
+                            lind_kill(cageid, 14);
+                            guard.cageid = None;
                         }
                     },
                 }
 
-                *guard.is_ticking = false;
+                guard.is_ticking = false;
             }
 
             Mutex::unlock(guard);
