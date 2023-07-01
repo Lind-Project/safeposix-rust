cb89eadf696ceb7e93b8a45c6d95ab85641209a7
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-06-30 22:30:58 -0400

    Add pipe2 test

--- src/tests/pipe_tests.rs
+++ src/tests/pipe_tests.rs
@@ -105,4 +105,77 @@ pub mod pipe_tests {
 
         lindrustfinalize();
     }
+
+    pub fn ut_lind_fs_pipe2() {
+
+        let byte_chunk: usize = 131072;
+        let num_writes: usize = 8192;
+        
+        lindrustinit(0);
+    
+        let cage1 = {CAGE_TABLE.get(&1).unwrap().clone()};
+    
+        // Pipe creation
+        let mut pipefds = PipeArray {readfd: -1, writefd: -1};
+        let flags = 0; // replace with desired flags for pipe2
+        // pipe2
+        assert_eq!(cage1.pipe2_syscall(&mut pipefds, flags), 0); 
+        assert_eq!(cage1.fork_syscall(2), 0);
+    
+        // Reading process 
+        let sender = std::thread::spawn(move || {
+    
+            let cage2 = {CAGE_TABLE.get(&2).unwrap().clone()};
+    
+            assert_eq!(cage2.close_syscall(pipefds.writefd), 0);
+            assert_eq!(cage2.dup2_syscall(pipefds.readfd, 0), 0);
+            assert_eq!(cage2.close_syscall(pipefds.readfd), 0);
+    
+            let mut bytes_read: usize = 1;
+    
+            let mut buf: Vec<u8> = Vec::with_capacity(byte_chunk * num_writes);
+            let mut bufptr = buf.as_mut_ptr();
+            let mut buflen: usize = 0;
+    
+            while bytes_read != 0 {
+                bytes_read = cage2.read_syscall(0, bufptr, byte_chunk) as usize;
+                unsafe {
+                    bufptr = bufptr.add(bytes_read);
+                    buf.set_len(buflen + bytes_read);
+                    buflen += bytes_read;
+                }
+            }
+            assert_eq!(cage2.close_syscall(0), 0);
+    
+            assert_eq!(cage2.exit_syscall(EXIT_SUCCESS), EXIT_SUCCESS);
+    
+        });
+        
+        assert_eq!(cage1.close_syscall(pipefds.readfd), 0);
+        assert_eq!(cage1.dup2_syscall(pipefds.writefd, 1), 1);
+        assert_eq!(cage1.close_syscall(pipefds.writefd), 0);
+    
+        let filefd = cage1.open_syscall("test1gb.txt", O_RDONLY, S_IRWXA);
+        
+        for _i in 0..num_writes {
+    
+            let mut buf: Vec<u8> = Vec::with_capacity(byte_chunk);
+            let bufptr = buf.as_mut_ptr();
+            unsafe { buf.set_len(byte_chunk); }
+    
+            cage1.read_syscall(filefd, bufptr, byte_chunk);
+            cage1.write_syscall(1, bufptr, byte_chunk);
+        }
+        assert_eq!(cage1.close_syscall(filefd), 0);
+    
+        assert_eq!(cage1.close_syscall(1), 0);
+    
+        sender.join().unwrap();
+    
+        assert_eq!(cage1.exit_syscall(EXIT_SUCCESS), EXIT_SUCCESS);
+    
+        lindrustfinalize();
+    }
+    
+    
 }
