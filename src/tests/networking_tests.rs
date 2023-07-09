033ca8cc3d8b1ff47d87462066103bda2a0dfae8
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-07-09 17:47:37 -0400

    Add epoll tests

--- src/tests/networking_tests.rs
+++ src/tests/networking_tests.rs
@@ -1405,4 +1405,172 @@ pub mod net_tests {
         lindrustfinalize();
     }
     
+    /* Creates an epoll instance, registers the server socket and file descriptor with epoll, and then wait for events using 
+    epoll_wait_syscall(). It handles the events based on their types (POLLIN or POLLOUT) and performs the necessary operations
+    like accepting new connections, sending/receiving data, and modifying the event flags */
+    pub fn ut_lind_net_epoll() {
+        lindrustinit(0);
+        let cage = interface::cagetable_getref(1);
+
+        let filefd = cage.open_syscall("/netpolltest.txt", O_CREAT | O_EXCL | O_RDWR, S_IRWXA);
+        assert!(filefd > 0);
+
+        let serversockfd = cage.socket_syscall(AF_INET, SOCK_STREAM, 0);
+        let clientsockfd1 = cage.socket_syscall(AF_INET, SOCK_STREAM, 0);
+        let clientsockfd2 = cage.socket_syscall(AF_INET, SOCK_STREAM, 0);
+
+        // Create and set up the file descriptor and sockets
+        let port: u16 = 53009;
+        let sockaddr = interface::SockaddrV4 {
+            sin_family: AF_INET as u16,
+            sin_port: port.to_be(),
+            sin_addr: interface::V4Addr {
+                s_addr: u32::from_ne_bytes([127, 0, 0, 1]),
+            },
+            padding: 0,
+        };
+        let socket = interface::GenSockaddr::V4(sockaddr);
+        assert_eq!(cage.bind_syscall(serversockfd, &socket), 0);
+        assert_eq!(cage.listen_syscall(serversockfd, 4), 0);
+
+        // Bind and listen on the server socket
+        let serverpoll = interface::PollStruct {
+            fd: serversockfd,
+            events: POLLIN,
+            revents: 0,
+        };
+        let filepoll = interface::PollStruct {
+            fd: filefd,
+            events: POLLIN,
+            revents: 0,
+        };
+        let mut polled = vec![serverpoll, filepoll];
+
+        // Create a vector of PollStruct to track the events to be polled 
+        cage.fork_syscall(2);
+        // Client 1 connects to the server to send and recv data
+        let thread1 = interface::helper_thread(move || {
+            interface::sleep(interface::RustDuration::from_millis(30));
+            let cage2 = interface::cagetable_getref(2);
+            // Connect to server and send data
+            assert_eq!(cage2.connect_syscall(clientsockfd1, &socket), 0);
+            assert_eq!(cage2.send_syscall(clientsockfd1, str2cbuf(&"test"), 4, 0), 4);
+            // Wait for data processing, give it a longer pause time so that it can process all of the data received
+            interface::sleep(interface::RustDuration::from_millis(100));
+            // Close the server socket and exit the thread 
+            assert_eq!(cage2.close_syscall(serversockfd), 0);
+            cage2.exit_syscall(EXIT_SUCCESS);
+        });
+
+        cage.fork_syscall(3);
+        // Client 2 connects to the server to send and recv data
+        let thread2 = interface::helper_thread(move || {
+            interface::sleep(interface::RustDuration::from_millis(45));
+            let cage3 = interface::cagetable_getref(3);
+            // Connect to server and send data
+            assert_eq!(cage3.connect_syscall(clientsockfd2, &socket), 0);
+            assert_eq!(cage3.send_syscall(clientsockfd2, str2cbuf(&"test"), 4, 0), 4);
+
+            interface::sleep(interface::RustDuration::from_millis(100));
+            // Close the server socket and exit the thread
+            assert_eq!(cage3.close_syscall(serversockfd), 0);
+            cage3.exit_syscall(EXIT_SUCCESS);
+        });
+        
+        // Acting as the server and processing the request
+        let thread3 = interface::helper_thread(move || {
+            let epfd = cage.epoll_create_syscall(0);
+            assert!(epfd > 0);
+            // Create epoll instance
+            let mut event = libc::epoll_event {
+                events: libc::EPOLLIN,
+                u64: 0,
+            };
+            assert_eq!(
+                cage.epoll_ctl_syscall(epfd, libc::EPOLL_CTL_ADD, serversockfd, &mut event),
+                0
+            );
+            assert_eq!(
+                cage.epoll_ctl_syscall(epfd, libc::EPOLL_CTL_ADD, filefd, &mut event),
+                0
+            );
+            // Event processing loop
+            for _counter in 0..600 {
+                
+                let num_events = cage.epoll_wait_syscall(epfd, &mut polled, 1, Some(interface::RustDuration::ZERO));
+                assert!(num_events >= 0);
+    
+                // Wait for events using epoll_wait_syscall
+                for polledfile in &mut polled[..num_events as usize] {
+                    // Check for any activity in the input socket and if there are events ready for reading
+                    if polledfile.revents & POLLIN != 0 {
+                        // If the socket returned was listener socket, then there's a new connection
+                        if polledfile.fd == serversockfd {
+                            // Handle new connections
+                            let port: u16 = 53009;
+                            let sockaddr = interface::SockaddrV4 {
+                                sin_family: AF_INET as u16,
+                                sin_port: port.to_be(),
+                                sin_addr: interface::V4Addr {
+                                    s_addr: u32::from_ne_bytes([127, 0, 0, 1]),
+                                },
+                                padding: 0,
+                            };
+                            let mut addr = interface::GenSockaddr::V4(sockaddr); // 127.0.0.1 from bytes above
+                            let newsockfd = cage.accept_syscall(serversockfd, &mut addr);
+                            let mut event = interface::EpollEvent {
+                                events: EPOLLIN as u32,
+                                fd: newsockfd,
+                            };
+                            // Error raised to indicate that the socket file descriptor couldn't be added to the epoll instance
+                            assert_eq!(
+                                cage.epoll_ctl_syscall(epfd, EPOLL_CTL_ADD, newsockfd, &event),
+                                0
+                            );
+                        } else if polledfile.fd == filefd { // Handle writing to the file
+                            // Update 
+                            assert_eq!(cage.write_syscall(filefd, str2cbuf("test"), 4), 4);
+                            assert_eq!(cage.lseek_syscall(filefd, 0, SEEK_SET), 0);
+                            polledfile.events = POLLOUT;
+                        } else {
+                            // Handle receiving data from established connections
+                            let mut buf = sizecbuf(4);
+                            let recres = cage.recv_syscall(polledfile.fd, buf.as_mut_ptr(), 4, 0);
+                            assert_eq!(recres & !4, 0);
+                            if recres == 4 {
+                                assert_eq!(cbuf2str(&buf), "test");
+                                polledfile.events = POLLOUT;
+                            } else {
+                                assert_eq!(cage.close_syscall(polledfile.fd), 0);
+                            }
+                        }
+                    }
+    
+                    if polledfile.revents & POLLOUT != 0 {
+                        // Check if there are events ready for writing
+                        if polledfile.fd == filefd {
+                            // Handle reading from the file
+                            let mut read_buf1 = sizecbuf(4);
+                            assert_eq!(cage.read_syscall(filefd, read_buf1.as_mut_ptr(), 4), 4);
+                            assert_eq!(cbuf2str(&read_buf1), "test");
+                        } else {
+                            // Handle sending data over connections
+                            assert_eq!(cage.send_syscall(polledfile.fd, str2cbuf(&"test"), 4, 0), 4);
+                            polledfile.events = POLLIN;
+                        }
+                    }
+                }
+            }
+    
+            // Close the server socket and exit the thread
+            assert_eq!(cage.close_syscall(serversockfd), 0);
+            assert_eq!(cage.exit_syscall(EXIT_SUCCESS), EXIT_SUCCESS);
+        });
+
+        thread1.join().unwrapthread2.join().unwrap();
+        thread3.join().unwrap();
+
+        lindrustfinalize();
+    }
+
 }
