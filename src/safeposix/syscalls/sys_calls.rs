09beda07d05587219a2e7e09d9bdf97bea9e5c08
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2023-08-21 00:49:41 -0400

    fix

--- src/safeposix/syscalls/sys_calls.rs
+++ src/safeposix/syscalls/sys_calls.rs
@@ -106,13 +106,6 @@ impl Cage {
                         let socket_type = sockhandle.domain;
                         if socket_type == AF_UNIX {
                             if let Some(sockinfo) = &sockhandle.unix_info {
-                                // if let Some(sendpipe) = sockinfo.sendpipe.as_ref() {
-                                //     sendpipe.incr_ref(O_WRONLY);
-                                // } else if let Some(receivepipe) = sockinfo.receivepipe.as_ref() {
-                                //     receivepipe.incr_ref(O_RDONLY);
-                                // } else {
-                                //     return syscall_error(Errno::EINVAL, "fork", "socket not send and not read")
-                                // }
                                 if let Some(sendpipe) = sockinfo.sendpipe.as_ref() {
                                     sendpipe.incr_ref(O_WRONLY);
                                 }
