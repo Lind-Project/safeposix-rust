988b108e20d8b82a78976aae732df87080c39e2e
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-09-09 01:10:55 -0400

    copy from atomicU32

--- src/safeposix/syscalls/sys_calls.rs
+++ src/safeposix/syscalls/sys_calls.rs
@@ -139,7 +139,7 @@ impl Cage {
             let shared = semaphore.isShared.load(interface::RustAtomicOrdering::Relaxed);
             let cloneshared = interface::RustAtomicU32::new(0);
             cloneshared.store(semaphore.isShared.load(interface::RustAtomicOrdering::Relaxed), interface::RustAtomicOrdering::Relaxed);
-            let cloneval = interface::RustAtomicU32::new(0);
+            let cloneval = interface::RustAtomicBool::new(0);
             cloneval.store(semaphore.value.load(interface::RustAtomicOrdering::Relaxed), interface::RustAtomicOrdering::Relaxed);
             if shared {
                 let new_semaphore = RustSemaphore{
