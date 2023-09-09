bed0d6c2d372c2428ea37d87bcced6b63392028e
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-09-09 03:22:42 -0400

    test sem

--- src/tests/fs_tests.rs
+++ src/tests/fs_tests.rs
@@ -1061,8 +1061,9 @@ pub mod fs_tests {
         // which is already allocated in a certain memory area when cage initialized
         let ret_init = cage1.sem_init_syscall(1 as u32, 0, 1);
         assert_eq!(ret_init, 0);
-        assert_eq!("FF", "");
+        
         assert_eq!(cage1.sem_wait_syscall(1 as u32), 0);
+        assert_eq!("FF", "");
         assert_eq!(cage1.sem_post_syscall(1 as u32), 0);
         assert_eq!(cage1.sem_destroy_syscall(1 as u32), 0);
 
