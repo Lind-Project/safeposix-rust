4f7480a52292adcaf2d31625d6cf7fbfbd6e6a9b
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-09-18 21:40:04 -0400

    test para

--- src/tests/fs_tests.rs
+++ src/tests/fs_tests.rs
@@ -1066,10 +1066,9 @@ pub mod fs_tests {
         assert_ne!(shmatret, -1);
         // Initialize the semaphore with shared between process
         let ret_init = cage.sem_init_syscall(shmatret as u32, 1, 1);
-        assert_eq!(shmatret as u32, 0);
         assert_eq!(ret_init, 0);
         let ret_get = cage.sem_getvalue_syscall(shmatret as u32);
-        assert_eq!(ret_get, 0);
+        assert!(ret_get >= 0, "Wrong sem_getvalue");
         // Fork child process
         assert_eq!(cage.fork_syscall(2), 0);
         // Child process
