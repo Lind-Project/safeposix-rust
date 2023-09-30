a95a8889b6bd639340cd43f45dc295db26cc5d6e
Rushang Gajjal <rushang101@gmail.com>
2023-09-29 21:12:28 -0400

    fix: break the loop in RustSemaphore.lock() (#193)
    Signed-off-by: RusherRG <rushang101@gmail.com>

--- src/interface/misc.rs
+++ src/interface/misc.rs
@@ -430,6 +430,7 @@ impl RustSemaphore {
             } else {
                 // decrement the value
                 *value = if *value > 0 { *value - 1 } else { 0 };
+                break;
             }
         }
     }
