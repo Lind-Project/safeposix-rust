18ca32361c04d154f203a4b47bbef3a805248bd9
Nicholas Renner <nicholassrenner@gmail.com>
2023-10-24 16:03:14 -0400

    fix warning

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -1141,7 +1141,7 @@ impl Cage {
             if fdguard.is_some() {
                 drop(fdguard);
                 //close the fd in the way of the new fd. If an error is returned from the helper, return the error, else continue to end
-                let close_result = Self::_close_helper_inner(&self, newfd);
+                let _close_result = Self::_close_helper_inner(&self, newfd);
                 // mirror the implementation of linux, ignore the potential error of the close here
             } else { drop(fdguard); }
             fdguard = self.filedescriptortable[newfd as usize].write();
