b81fa1c46a05b097e6209d880d44cda6133adcba
Alice W. <40227173+Yaxuan-w@users.noreply.github.com>
2023-11-28 18:51:11 -0500

    Update openat

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -148,7 +148,7 @@ impl Cage {
         if interface::RustPath::new(path).is_absolute() {
             return Self::open_syscall(&self, &path, flags, mode);
         } else if dirfd == AT_FDCWD {
-            let current_path = self.cwd.read().to_str();
+            let current_path = self.cwd.read().to_str().unwrap();
             let truepath = current_path + path;
             return Self::open_syscall(&self, &truepath, flags, mode);
         } else {
