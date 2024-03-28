c2ee3d2a8886080dd2cb10a750a7a5a84d0c5ab3
Yashaswi Makula <yashaswi.makula@gmail.com>
2024-03-28 14:56:03 -0400

    Add semaphores (#191)
    * add semaphore
    
    * add semaphore
    
    * add semaphore
    
    * add semaphore
    
    * add semaphore
    
    * add semaphore
    
    * change to loop
    
    * copy from atomicU32
    
    * copy from atomicU32
    
    * copy from atomicU32
    
    * copy from atomicU32
    
    * copy from atomicU32
    
    * copy from atomicU32
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * comment sem
    
    * comment sem
    
    * comment sem
    
    * comment sem
    
    * test 2 process sem
    
    * test 2 process sem
    
    * test 2 process sem
    
    * test 2 process sem
    
    * test 2 process sem
    
    * test 2 process sem
    
    * test 2 process sem
    
    * test 2 process sem
    
    * test 2 process sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * modify sem
    
    * shared implementation, need to debug
    
    * fix borrowing and semantic errors
    
    Signed-off-by: RusherRG <rushang101@gmail.com>
    
    * shared implementation, need to debug
    
    * fix lock
    
    * fix lock
    
    * fix params
    
    * add sem_trywait/sem_timedwait
    
    * add sem_trywait/sem_timedwait
    
    * test sem_trywait/timedwait
    
    * fix references
    
    * fix references
    
    * test sem_trywait/timedwait
    
    * fix address option
    
    * test sem_trywait/timedwait
    
    * test sem_trywait
    
    * test para
    
    * test para
    
    * test para
    
    * test para
    
    * test para
    
    * test para
    
    * test para
    
    * test para
    
    * test para
    
    * fix errno test
    
    * fix errno test
    
    * test add value check
    
    * test add value check
    
    * test add value check
    
    * test sem_post
    
    * test sem_post
    
    * test sem_post
    
    * test sem_post
    
    * test sem_post
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * test sem
    
    * fix: copy semaphore table to child cage even when pshared is zero
    
    Signed-off-by: RusherRG <rushang101@gmail.com>
    
    * refactor: remove use of enumerate
    
    Signed-off-by: RusherRG <rushang101@gmail.com>
    
    ---------
    
    Signed-off-by: RusherRG <rushang101@gmail.com>
    Co-authored-by: Yaxuan-w <wenyaxuan0925@outlook.com>
    Co-authored-by: RusherRG <rushang101@gmail.com>

--- src/safeposix/syscalls/sys_calls.rs
+++ src/safeposix/syscalls/sys_calls.rs
@@ -23,6 +23,7 @@ impl Cage {
                     let segment = occupied.get_mut();
                     segment.shminfo.shm_nattch -= 1;
                     segment.shminfo.shm_dtime = interface::timestamp() as isize;
+                    segment.attached_cages.remove(&self.cageid);
             
                     if segment.rmid && segment.shminfo.shm_nattch == 0 {
                         let key = segment.key;
@@ -127,6 +128,16 @@ impl Cage {
             } else {panic!("We changed from a directory that was not a directory in chdir!");}
         } else {panic!("We changed from a directory that was not a directory in chdir!");}
 
+        /* 
+        *  Construct a new semaphore table in child cage which equals to the one in the parent cage
+        */
+        let semtable = &self.sem_table;
+        let new_semtable: interface::RustHashMap<u32, interface::RustRfc<interface::RustSemaphore>> = interface::RustHashMap::new();
+        // Loop all pairs
+        for pair in semtable.iter() {
+            new_semtable.insert((*pair.key()).clone(), pair.value().clone());
+        }
+
         let cageobj = Cage {
             cageid: child_cageid, cwd: interface::RustLock::new(self.cwd.read().clone()), parent: self.cageid,
             filedescriptortable: newfdtable,
@@ -140,6 +151,7 @@ impl Cage {
             mutex_table: interface::RustLock::new(new_mutex_table),
             cv_table: interface::RustLock::new(new_cv_table),
             thread_table: interface::RustHashMap::new(),
+            sem_table: new_semtable,
         };
 
         let shmtable = &SHM_METADATA.shmtable;
@@ -147,6 +159,8 @@ impl Cage {
         for rev_mapping in cageobj.rev_shm.lock().iter() {
             let mut shment = shmtable.get_mut(&rev_mapping.1).unwrap();
             shment.shminfo.shm_nattch += 1;
+            let refs = shment.attached_cages.get(&self.cageid).unwrap();
+            shment.attached_cages.insert(child_cageid, *refs);
         }
         drop(shmtable);
         interface::cagetable_insert(child_cageid, cageobj);
@@ -199,6 +213,7 @@ impl Cage {
             mutex_table: interface::RustLock::new(vec!()),
             cv_table: interface::RustLock::new(vec!()),
             thread_table: interface::RustHashMap::new(),
+            sem_table: interface::RustHashMap::new(),
         };
         //wasteful clone of fdtable, but mutability constraints exist
 
