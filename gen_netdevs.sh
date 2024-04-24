384528062f0fc7d6be73197e23136eea783b507c
Yashaswi Makula <yashaswi.makula@gmail.com>
2024-04-24 13:15:02 -0400

    fixes for running lind install (#219)

--- gen_netdevs.sh
+++ gen_netdevs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 WORKDIR=$(pwd)  # Set working directory to the output of pwd
 FILE="$WORKDIR/gen_netdevs"  # Use the full path for the file
+PARENTPATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P ) # needs the source location when being used in lind-build.
+
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else 
     echo "$FILE does not exist. Compiling."
-    gcc "$WORKDIR/gen_netdevs.c" -o "$WORKDIR/gen_netdevs"
+    gcc "$PARENTPATH/gen_netdevs.c" -o "$WORKDIR/gen_netdevs"
     if [ $? -ne 0 ]; then  # Check if gcc succeeded
         echo "Compilation failed."
         exit 1
