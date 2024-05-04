8556261e7f46bb2797ab41b3158f9e1590e8ce10
Nicholas Renner <nicholassrenner@gmail.com>
2024-05-04 14:27:51 -0400

    update comment

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -1828,7 +1828,7 @@ impl Cage {
                     panic!("Somehow a normal file with an fd was truncated but there was no file object in rustposix?");
                 } else {
                     let sysfilename = format!("{}{}", FILEDATAPREFIX, inodenum);
-                    tempbind = interface::openfile(sysfilename, filesize).unwrap(); // create new file
+                    tempbind = interface::openfile(sysfilename, filesize).unwrap(); // open file with size given from inode
                     close_on_exit = true;
                     &mut tempbind
                 };
