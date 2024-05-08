e0a8f6e0c5bc90958622e615bf88570a9eb95f80
Alice W. <wenyaxuan0925@outlook.com>
2024-05-08 13:05:57 -0400

    Fixing type casting: conditional compile

--- benches/fs_read_write_seek.rs
+++ benches/fs_read_write_seek.rs
@@ -81,12 +81,14 @@ pub fn run_benchmark(c: &mut Criterion) {
     for buflen in [1,64,1024,65536].iter() {
         let fd: c_int;
 
+        let c_str = CString::new("/tmp/foo").unwrap();
+        
         #[cfg(target_os = "macos")]
         let path = c_str.into_raw() as *const u8;
 
         #[cfg(not(target_os = "macos"))]
         let path = c_str.into_raw() as *const i8;
-        
+
         unsafe {
             fd = libc::open(path,O_CREAT | O_TRUNC | O_WRONLY,S_IRWXA);
         }
