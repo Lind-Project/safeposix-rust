cd040133507b920c50e0b8701f72d5f79d1f2700
Alice W. <wenyaxuan0925@outlook.com>
2024-05-20 11:11:21 -0400

    Add file offset print

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -749,8 +749,12 @@ impl Cage {
                             let position = normalfile_filedesc_obj.position;
                             let fileobject =
                                 FILEOBJECTTABLE.get(&normalfile_filedesc_obj.inode).unwrap();
-
+                            
+                            println!("[DEBUG - read] :{:?}", fname);
+                            std::io::stdout().flush().unwrap();
                             println!("[DEBUG] File Descriptor Offset: {:?}", position);
+                            std::io::stdout().flush().unwrap();
+                            
                             if let Ok(bytesread) = fileobject.readat(buf, count, position) {
                                 //move position forward by the number of bytes we've read
 
