d1631c0797298b13aef6984d2e2ba8ecc13ba746
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2024-05-22 23:43:02 +0530

    test udp_sample

--- src/tests/networking_tests.rs
+++ src/tests/networking_tests.rs
@@ -23,7 +23,7 @@ pub mod net_tests {
         ut_lind_net_socketoptions();
         ut_lind_net_socketpair();
         ut_lind_net_udp_bad_bind();
-        // ut_lind_net_udp_simple();
+        ut_lind_net_udp_simple();
         // ut_lind_net_udp_connect();
         // ut_lind_net_gethostname();
         // ut_lind_net_dns_rootserver_ping();
