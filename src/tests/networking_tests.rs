fc92921b16f5475d84317f1922a93e950d954b65
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2024-05-22 23:44:48 +0530

    test dns

--- src/tests/networking_tests.rs
+++ src/tests/networking_tests.rs
@@ -26,7 +26,7 @@ pub mod net_tests {
         ut_lind_net_udp_simple();
         ut_lind_net_udp_connect();
         ut_lind_net_gethostname();
-        // ut_lind_net_dns_rootserver_ping();
+        ut_lind_net_dns_rootserver_ping();
         // ut_lind_net_domain_socket();
         // ut_lind_net_epoll();
         // ut_lind_net_writev();
