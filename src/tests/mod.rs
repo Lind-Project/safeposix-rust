fb3f419c42e2ec7c5207d0f9f3b3c8d54f42bb86
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2024-06-12 01:54:09 +0530

    update random_port

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -94,8 +94,8 @@ pub fn cbuf2str(buf: &[u8]) -> &str {
 }
 
 fn is_port_available(port: u16) -> bool {
-    TcpListener::bind(("127.0.0.1", port)).is_ok()
-    //UdpSocket::bind(("127.0.0.1", port)).is_ok()
+    TcpListener::bind(("127.0.0.1", port)).is_ok()&&
+    UdpSocket::bind(("127.0.0.1", port)).is_ok()
 }
 
 pub fn generate_random_port() -> u16 {
