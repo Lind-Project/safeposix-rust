6a6b6b39c6e3072eb47226869e124fab0a9a472e
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2024-06-12 01:14:46 +0530

    test unused port only

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -98,11 +98,12 @@ fn is_port_available(port: u16) -> bool {
 }
 
 pub fn generate_random_port() -> u16 {
-    for _ in 0..100 {
-        let port = rng.gen_range(49152..=65535);
+    let mut rng = rand::thread_rng();
+    
+    loop {
+        let port = rng.gen_range(49152..65535);
         if is_port_available(port) {
             return port;
         }
     }
-    None
 }
