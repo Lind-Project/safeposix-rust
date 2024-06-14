4f1f25193f766609f55d922ca7788f30200aed58
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2024-06-14 18:47:52 +0530

    test new function

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -3587,25 +3587,12 @@ impl Cage {
     /*
      * Take only sem_t *sem as argument, and return int *sval
      */
-    pub fn sem_getvalue_syscall(&self, sem_handle: u32) -> i32 {
-        let semtable = &self.sem_table;
-        if let Some(sementry) = semtable.get_mut(&sem_handle) {
-            let semaphore = sementry.clone();
-            drop(sementry);
-            println!("semaphore: and pointer is {:?}" , &semaphore as *const Arc<interface::RustSemaphore>);
-            return semaphore.get_value();
-        }
-        return syscall_error(
-            Errno::EINVAL,
-            "sem_getvalue",
-            "sem is not a valid semaphore",
-        );
-    }
-
     // pub fn sem_getvalue_syscall(&self, sem_handle: u32) -> i32 {
     //     let semtable = &self.sem_table;
-    //     if let Some(semaphore) = semtable.get(&sem_handle) {
-    //         println!("semaphore: and pointer is {:?}", &**semaphore as *const _);
+    //     if let Some(sementry) = semtable.get_mut(&sem_handle) {
+    //         let semaphore = sementry.clone();
+    //         drop(sementry);
+    //         println!("semaphore: and pointer is {:?}" , &semaphore as *const Arc<interface::RustSemaphore>);
     //         return semaphore.get_value();
     //     }
     //     return syscall_error(
@@ -3614,6 +3601,19 @@ impl Cage {
     //         "sem is not a valid semaphore",
     //     );
     // }
+
+    pub fn sem_getvalue_syscall(&self, sem_handle: u32) -> i32 {
+        let semtable = &self.sem_table;
+        if let Some(semaphore) = semtable.get(&sem_handle) {
+            println!("semaphore: and pointer is {:?}", &**semaphore as *const _);
+            return semaphore.get_value();
+        }
+        return syscall_error(
+            Errno::EINVAL,
+            "sem_getvalue",
+            "sem is not a valid semaphore",
+        );
+    }
 
     pub fn sem_trywait_syscall(&self, sem_handle: u32) -> i32 {
         let semtable = &self.sem_table;
