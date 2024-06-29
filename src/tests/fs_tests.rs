8dea1c21c8b8d9fd1ed208e0196ab12027885880
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2024-06-29 19:53:38 +0530

    add empty dir test

--- src/tests/fs_tests.rs
+++ src/tests/fs_tests.rs
@@ -1465,7 +1465,7 @@ pub mod fs_tests {
         lindrustfinalize();
     }
 
-    #[test]
+    // #[test]
     // fn ut_lind_fs_getdents_empty_directory() {
     //     let _thelock = setup::lock_and_init();
     //     let cage = interface::cagetable_getref(1);
