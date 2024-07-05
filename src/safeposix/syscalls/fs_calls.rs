c9082f0c9d8680a3bd76429a7ac7851fe01ccf93
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2024-07-05 19:55:31 +0530

    update file

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -185,7 +185,7 @@ impl Cage {
             inode: inodenum,
             flags: flags & allowmask,
             advlock: interface::RustRfc::new(interface::AdvisoryLock::new()),
-            file: Some(Arc::new(RwLock::new(file))),
+            file: Some(Arc::new(RwLock::new(FileDescriptor::File(file)))),
         }
     }
 
