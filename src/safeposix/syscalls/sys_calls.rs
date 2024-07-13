4c3bf2188050a59f420f396ae16ce86c7e1077f3
lind <lind@nyu.edu>
2024-07-13 20:06:17 +0000

    removed decrement def

--- src/safeposix/syscalls/sys_calls.rs
+++ src/safeposix/syscalls/sys_calls.rs
@@ -527,8 +527,6 @@ impl Cage {
         //Read the current working directory (acquire a lock)
         let cwd_container = self.cwd.read();
         //For all inodes to which the current cage object points to
-        //Decrement their reference count
-        decref_dir(&*cwd_container);
         //Remove the current cage object from the cage table
         interface::cagetable_remove(self.cageid);
 
