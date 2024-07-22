92989dde346d385fdea68f9179f5301185a7278d
lind <lind@nyu.edu>
2024-07-22 22:09:33 +0000

    minor changes

--- src/tests/fs_tests.rs
+++ src/tests/fs_tests.rs
@@ -7,7 +7,7 @@ pub mod fs_tests {
     use crate::safeposix::syscalls::fs_calls::*;
     use crate::safeposix::{cage::*, dispatcher::*, filesystem};
     use libc::{c_void, EINVAL};
-    use std::borrow::Borrow;
+    use std::borrow::{Borrow, BorrowMut};
     use std::fs::OpenOptions;
     use std::os::unix::fs::PermissionsExt;
 
@@ -4083,8 +4083,11 @@ pub mod fs_tests {
         // Assert that the return address is valid
         assert_ne!(shmret,-1); 
 
-        // Assert that the functin returns an appropriate error when passed an invalid shmid 
+        // Assert that the function returns an appropriate error when passed an invalid shmid 
         assert_eq!(cage.shmat_syscall(shmid+10, 0xfffff000 as *mut u8, 0),-(Errno::EINVAL as i32));
+
+    
+
         lindrustfinalize();
     }
 }
