5574edbba38bb956903100fde165bd41ad809f04
Nicholas Renner <nicholassrenner@gmail.com>
2025-02-18 22:53:28 -0500

    kill fix

--- src/safeposix/syscalls/sys_calls.rs
+++ src/safeposix/syscalls/sys_calls.rs
@@ -777,6 +777,7 @@ impl Cage {
         if let Some(cage) = interface::cagetable_getref_opt(cage_id as u64) {
             let threadid = cage.main_threadid.load(interface::RustAtomicOrdering::Relaxed);
             if threadid == 0 { return syscall_error(Errno::ESRCH, "kill", "Target cage does not exist"); }
+            
             interface::lind_threadkill(
                 threadid,
                 sig,
